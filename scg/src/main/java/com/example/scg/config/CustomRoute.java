eb061b50a55d416447bc9b6df7684362b2e5902c
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-12 16:08:08 +0900

    test load balancing

--- scg/src/main/java/com/example/scg/config/CustomRoute.java
+++ scg/src/main/java/com/example/scg/config/CustomRoute.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class CustomRoute {
 
-    @Bean
+//    @Bean
     public RouteLocator cRoute(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("client1", r->r.path("/client1/**")
